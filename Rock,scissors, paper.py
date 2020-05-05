afe3884cdcf99059598ba626008d27f56c8a933a
phoebe2709 <61206950+phoebe2709@users.noreply.github.com>
2020-05-05 20:28:29 +0100

    Add files via upload

---
+++ Rock,scissors, paper.py
@@ -0,0 +1,34 @@
+import random
+
+def random_choice():
+    choice_number = random.randint(1,3)
+
+    if choice_number == 1:
+        choice = "rock"
+    elif choice_number == 2:
+        choice = "scissors"
+    else:
+        choice = "paper"
+    return choice
+
+my_choice = input("Choose rock, scissors or paper:")
+opponent_choice = random_choice()
+
+print("Your opponent chose {}".format(opponent_choice))
+
+if my_choice == "rock" and opponent_choice == "scissors" :
+    print("You win")
+
+elif my_choice == "scissors" and opponent_choice == "paper" :
+    print("You win")
+
+elif my_choice == "paper" and opponent_choice == "rock" :
+    print("You win")
+
+elif my_choice == opponent_choice :
+    print("You draw")
+
+else:
+    print("You lose")
+
+
